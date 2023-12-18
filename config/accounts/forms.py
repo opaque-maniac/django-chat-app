385bd57bb09f67ec4d438c1eebb8d0369c2c197b
opaque-maniac <mbithimark8@gmail.com>
2023-12-18 17:34:51 +0300

    Re wrote the user creation form

--- config/accounts/forms.py
+++ config/accounts/forms.py
@@ -1,18 +1,24 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from .models import CustomUser
 
-class RegsiterForm(forms.ModelForm):
-    class Meta:
+class RegsiterForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
         model = CustomUser
-        fields = ['email', 'first_name', 'last_name', 'password']
+        fields = ['email', 'first_name', 'last_name', 'password1', 'password2']
         labels = {
+            'email': 'Email Address',
             'first_name': 'First Name',
             'last_name': 'Last Name',
-            'email': 'Email Address',
-            'password': 'Password'
+            'password1': 'Password',
+            'password2': 'Confirm Password'
         }
         widgets = {
+            'email': forms.EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Enter Email Address'
+            }),
             'first_name': forms.TextInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Enter First Name'
@@ -21,13 +27,13 @@ class RegsiterForm(forms.ModelForm):
                 'class': 'form-control',
                 'placeholder': 'Enter Last Name'
             }),
-            'email': forms.EmailInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Enter Email Address'
-            }),
-            'password': forms.PasswordInput(attrs={
+            'password1': forms.PasswordInput(attrs={
                 'class': 'form-control',
                 'placeholder': 'Enter Password'
+            }),
+            'password2': forms.PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirm Password'
             }),
         }
 
