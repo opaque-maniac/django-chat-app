71e07af48ec333a2d0489dc9664b34781a9cc824
opaque-maniac <mbithimark8@gmail.com>
2023-12-20 00:14:27 +0300

    Sorry

--- config/chat/static/chat/js/individual_chat.js
+++ config/chat/static/chat/js/individual_chat.js
@@ -6,22 +6,17 @@ $(document).ready(function() {
           type: 'POST',
           data: { 'content': messageContent, csrfmiddlewaretoken: '{{ csrf_token }}' },
           success: function(data) {
-            // Optionally handle success response
-            // For example, clear the input field
             $('#message-input').val('');
           },
         });
       }
     
-      // Update messages every 5 seconds (adjust as needed)
       setInterval(updateMessages, 5000);
     
-      // Function to update messages
       function updateMessages() {
         $.ajax({
           url: "{% url 'update_messages' conversation.id %}",
           success: function(data) {
-            // Update the message list with new messages
             $("#message-list").html(data);
           },
         });
